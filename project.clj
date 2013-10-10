6ec45e8db9b4472760647598c465128e5444170b
Joel Holdbrooks <cjholdbrooks@gmail.com>
2013-10-10 15:00:38 -0700

    Set development version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-garden "0.1.0"
+(defproject lein-garden "0.1.1-SNAPSHOT"
   :description "A Leiningen plugin for automatically compiling Garden stylesheets"
   :url "https://github.com/noprompt/lein-garden"
   :license {:name "Unlicense"
