2ee5c80cb6b78d46dd0d23b549ec3b75697b25b4
Joel Holdbrooks <cjholdbrooks@gmail.com>
2014-01-26 12:09:02 -0800

    Update garden version

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Unlicense"
             :url "http://unlicense.org/UNLICENSE"}
   :eval-in-leiningen true
-  :dependencies [[garden "1.1.2"]
+  :dependencies [[garden "1.1.5"]
                  [me.raynes/fs "1.4.4"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
                  [clojure-watch "0.1.9"]])
